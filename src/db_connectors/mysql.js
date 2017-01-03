55fb7f7d5028bb7c5b234c2ca645500e006f0b5b
Kristoffer Furst <kristoffer.furst@gmail.com>
2017-01-03 16:43:29 +0100

    get table info with key constraints now works

--- src/db_connectors/mysql.js
+++ src/db_connectors/mysql.js
@@ -16,21 +16,15 @@ function getTableDefinition(dbSchema, tableName) {
 	connection.query(
 			'select * ' +
 			'From '+
-				'INFORMATION_SCHEMA.COLUMNS As C '+
+				'INFORMATION_SCHEMA.COLUMNS As C Left Join '+
+				'INFORMATION_SCHEMA.KEY_COLUMN_USAGE As KCU '+
+			'On '+
+				'C.TABLE_NAME = KCU.TABLE_NAME And '+
+				'C.TABLE_SCHEMA = KCU.TABLE_SCHEMA And '+
+				'C.COLUMN_NAME = KCU.COLUMN_NAME  '+
 			'Where '+
 				'C.TABLE_NAME = \'' + tableName +'\' and '+
 				'C.TABLE_SCHEMA = \'' + dbSchema +'\'',
-			function(error, rows, fields) {
-		console.log(rows);
-	});
-
-	connection.query(
-			'select * ' +
-			'From '+
-				'INFORMATION_SCHEMA.KEY_COLUMN_USAGE As TC '+
-			'Where '+
-				'TC.TABLE_NAME = \'' + tableName +'\' and '+
-				'TC.TABLE_SCHEMA = \'' + dbSchema +'\'',
 			function(error, rows, fields) {
 		console.log(rows);
 	});
