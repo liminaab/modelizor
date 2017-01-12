18a5995e26bddcf7be6a09b9b0e1830a98e462bb
Kristoffer Furst <kristoffer.furst@gmail.com>
2017-01-12 18:46:42 +0100

    basic structure for file templates

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var settings = {
+var dbSettings = {
 	host:'localhost',
 	port:'8889',
 	user:'root',
 	password:'root',
 	database:'modelizor_test'
 };
+var fileTemplateFolder = 'java';
 
+/*
 var callback =  function(result) {
 	console.log(result);
 };
-
-var dbConnection = require('./src/db_connectors/mysql.js').connect(settings);
-
+var dbConnection = require('./src/db_connectors/mysql.js').connect(dbSettings);
 dbConnection.getTableDefinition('modelizor_test', 'pets', callback);
+*/
 
+
+var generatorModel = require('./file_templates/' + fileTemplateFolder + '/model.js');
+console.log(generatorModel.getBody());
---
+++ file_templates/java/model.js
@@ -0,0 +1,11 @@
+
+
+function getBody() {
+	return '' +
+	'public class Hej {\n' +
+	'	...\n' +
+	'}\n' +
+	'';
+}
+
+exports.getBody = getBody;
